511e3989c44acd7795659c7dd2bb35358291c9fe
Franziska Pendzialek <franziska.pendzialek@outlook.com>
2021-12-18 19:26:46 +0100

    update Menu Layout

--- components/layout/menu/MenuLayout.jsx
+++ components/layout/menu/MenuLayout.jsx
@@ -1,19 +1,65 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React, { useContext } from 'react'
+import MediaQuery from 'react-responsive'
 import MenuContext from '../../../util/context/MenuContext'
+import { mdQuery } from '../../../util/helpers'
+import hamburgerIcon from '../../../public/assets/hamburger.svg'
+import ArrowLeft from '../../../public/assets/arrow-left.svg'
 
 const MenuLayout = () => {
   const { setIsMenuOpen, isMenuOpen } = useContext(MenuContext)
+
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <>
-      <nav className="flex flex-row items-center justify-evenly font-bold text-xl my-4">
-        <Link href="/">
-          <a className="underline-gradient">About Fran</a>
-        </Link>
-        <Link href="/contact">
-          <a className="underline-gradient">Contact Me</a>
-        </Link>
-      </nav>
+      <MediaQuery maxWidth={mdQuery}>
+        <nav className="ml-6 mt-6">
+          {isMenuOpen ? (
+            <button onClick={handleClick}>
+              <Image
+                width={20}
+                height={20}
+                src={ArrowLeft}
+                alt="hamburger-menu"
+              />
+            </button>
+          ) : (
+            <button onClick={handleClick}>
+              <Image
+                width={40}
+                height={40}
+                src={hamburgerIcon}
+                alt="hamburger-menu"
+              />
+            </button>
+          )}
+        </nav>
+
+        {isMenuOpen && (
+          <div className="bg-white flex flex-col items-center">
+            <Link href="/">
+              <a className="underline-gradient my-2">About Fran</a>
+            </Link>
+            <Link href="/contact">
+              <a className="underline-gradient my-2">Contact Me</a>
+            </Link>
+          </div>
+        )}
+      </MediaQuery>
+      <MediaQuery minWidth={mdQuery}>
+        <nav className="flex flex-row items-center justify-evenly font-bold text-xl my-4">
+          <Link href="/">
+            <a className="underline-gradient">About Fran</a>
+          </Link>
+          <Link href="/contact">
+            <a className="underline-gradient">Contact Me</a>
+          </Link>
+        </nav>
+      </MediaQuery>
     </>
   )
 }
---
+++ util/helpers.js
@@ -0,0 +1,11 @@
+/* ------- MEDIA BREAKPOINTS ------- */
+
+export const smQuery = 640
+
+export const mdQuery = 768
+
+export const lgQuery = 1024
+
+export const xlQuery = 1280
+
+export const xxlQuery = 1536
