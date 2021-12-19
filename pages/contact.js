4f1280204ee8bc716dfab9d73a74d538fd5a9fc7
Franziska Pendzialek <franziska.pendzialek@outlook.com>
2021-12-19 19:45:06 +0100

    Add embedded Typeform to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,22 @@
-import Head from 'next/head'
 import React from 'react'
 import Hero from '../components/Hero'
-import Footer from '../components/layout/Footer'
-import Menu from '../components/layout/menu/Menu'
+import { Widget } from '@typeform/embed-react'
 
 const Contact = () => {
+  const widgetContainerStyle = {
+    width: 500,
+    height: 600,
+  }
   return (
     <div>
       <Hero />
+      <div className="flex flex-col items-center py-28 gradient">
+        <Widget
+          id="UehmCZMU"
+          style={widgetContainerStyle}
+          className="contact-form"
+        />
+      </div>
     </div>
   )
 }
