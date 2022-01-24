21bccee9d3856231e40e7eee88d50c23569b2871
Franziska Pendzialek <franziska.pendzialek@outlook.com>
2022-01-24 12:54:00 +0100

    update Podcast Information

--- components/BrandHero.jsx
+++ components/BrandHero.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
 import BrandLogo from './../public/assets/brand-logo.png'
+import AppleIcon from './../public/assets/apple.svg'
+import SpotifyIcon from './../public/assets/spotify.svg'
 
 export const BrandHero = () => {
   return (
@@ -23,12 +25,24 @@ export const BrandHero = () => {
         understand! Find new motivation and start your journey in tech right
         here! ✨
       </div>
-      <a
-        className="button-gradient-small"
-        href="https://codingstuffandcoffee.com"
-      >
-        Go to Website
-      </a>
+      <p className="font-sans text-xl font-bold text-center py-10">
+        Listen to the podcast here:
+      </p>
+
+      <div className="flex flex-col md:flex-row justify-center items-center gap-6 bg-lilac py-2 px-4 rounded-lg">
+        <a
+          href="https://podcasts.apple.com/us/podcast/coding-stuff-coffee/id1602976086"
+          className="link"
+        >
+          <Image width={60} height={60} src={AppleIcon} alt="apple-music" />
+        </a>
+        <a
+          href="https://open.spotify.com/show/2iYHh6x5OQkHds74ub6zkC"
+          className="link"
+        >
+          <Image width={60} height={60} src={SpotifyIcon} alt="spotify" />
+        </a>
+      </div>
     </div>
   )
 }
